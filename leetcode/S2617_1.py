18be415aa2d9d3f37b8294914ca591cf3a7e1f0a
kelvin4587 <michael8101@126.com>
2024-03-28 17:20:38 +0800

    移动方块最短路径

--- leetcode/S2617_1.py
+++ leetcode/S2617_1.py
@@ -17,7 +17,24 @@ class Solution:
                 while row[i] and row[i][0][1] + grid[i][row[i][0][1]] < j:
                     heapq.heappop(row[i])
                 if row[i]:
-                    dist[i][j] = update(dist[i][j],dist[i][row[i][0][1]] + 1)
+                    dist[i][j] = update(dist[i][j], dist[i][row[i][0][1]] + 1)
 
                 while col[j] and col[j][0][1] + grid[col[j][0][1]][j] < i:
                     heapq.heappop(col[j])
+                if col[j]:
+                    dist[i][j] = update(dist[i][j], dist[col[j][0][1]][j] + 1)
+
+                if dist[i][j] != -1:
+                    heapq.heappush(row[i], (dist[i][j], j))
+                    heapq.heappush(col[j], (dist[i][j], i))
+        return dist[m - 1][n - 1]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    list1 = [[3, 4, 2, 1], [4, 2, 3, 1], [2, 1, 0, 0], [2, 4, 0, 0]]
+    print(s.minimumVisitedCells(list1))
+    list2 = [[3, 4, 2, 1], [4, 2, 1, 1], [2, 1, 1, 0], [3, 4, 1, 0]]
+    print(s.minimumVisitedCells(list2))
+    list3 = [[2,1,0],[1,0,0]]
+    print(s.minimumVisitedCells(list3))
